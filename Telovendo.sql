263e7aff4d5e6e1e0461c0dd4a54efae265c6f36
Ricardo Cea <ricardocea.audio@gmail.com>
2023-04-16 13:30:03 -0400

    Anado a rama main, parte de simon. Corrijo problemas de ejecución de código

--- Telovendo.sql
+++ Telovendo.sql
@@ -159,7 +159,7 @@ VALUES ( '0000010', 'Paolo ','Zarate Navarrete' , 56980809765,' rocas grande 230
 
 -- CREACION DE COLUMNAS NUEVAS
 ALTER TABLE cliente
-ADD COLUMN Total Pagado INT NOT NULL DEFAULT '0';
+ADD COLUMN Total_Pagado INT NOT NULL DEFAULT '0';
 
 ALTER TABLE vendedor
 ADD COLUMN Salario INT NOT NULL DEFAULT '410000';
@@ -168,29 +168,28 @@ ALTER TABLE Producto
 ADD COLUMN Precio INT NOT NULL DEFAULT '10000';
 
 -- CREACION DE PRODUCTOS NUEVOS SSD
-
 INSERT INTO producto (SKU, nombre, categoria, productor, cantidad_en_stock, Precio) 
-VALUES ('45565', 'Unidad SSD 500GB Sata3 2.5" CS900', 'SSD', 'PNY', '12', '42000')
+VALUES ('45565', 'Unidad SSD 500GB Sata3 2.5" CS900', 'SSD', 'PNY', '12', '42000');
 INSERT INTO producto (SKU, nombre, categoria, productor, cantidad_en_stock, Precio) 
-VALUES ('27163', 'Unidad SSD 480GB Sata3 2.5" A400" CS900', 'SSD', 'KINGSTON', '14', '32000')
+VALUES ('27163', 'Unidad SSD 480GB Sata3 2.5" A400" CS900', 'SSD', 'KINGSTON', '14', '32000');
 INSERT INTO producto (SKU, nombre, categoria, productor, cantidad_en_stock, Precio) 
-VALUES ('27162', 'Unidad SSD 240GB Sata3 2.5" A400" CS900', 'SSD', 'KINGSTON', '11', '22000')
+VALUES ('27162', 'Unidad SSD 240GB Sata3 2.5" A400" CS900', 'SSD', 'KINGSTON', '11', '22000');
 INSERT INTO producto (SKU, nombre, categoria, productor, cantidad_en_stock, Precio) 
-VALUES ('28150', 'Unidad SSD 500GB Sata3 2.5" MX500', 'SSD', 'CRUCIAL', '27', '62000')
+VALUES ('28150', 'Unidad SSD 500GB Sata3 2.5" MX500', 'SSD', 'CRUCIAL', '27', '62000');
 INSERT INTO producto (SKU, nombre, categoria, productor, cantidad_en_stock, Precio) 
-VALUES ('30966', 'Unidad SSD 240GB Sata3 2.5" BX500', 'SSD', 'CRUCIAL', '36', '21000')
+VALUES ('30966', 'Unidad SSD 240GB Sata3 2.5" BX500', 'SSD', 'CRUCIAL', '36', '21000');
 
 -- CREACION DE PRODUCTOS NUEVOS RAM
-
 INSERT INTO producto (SKU, nombre, categoria, productor, cantidad_en_stock, Precio) 
-VALUES ('33918', 'DDR4 4GB 2666MHz Value RAM', 'RAM', 'CRUCIAL', '33', '22000')
+VALUES ('33918', 'DDR4 4GB 2666MHz Value RAM', 'RAM', 'CRUCIAL', '33', '22000');
 INSERT INTO producto (SKU, nombre, categoria, productor, cantidad_en_stock, Precio) 
-VALUES ('43964', 'DDR4 8GB 2666MHz Beast', 'RAM', 'KINGSTON', '28', '30000')
+VALUES ('43964', 'DDR4 8GB 2666MHz Beast', 'RAM', 'KINGSTON', '28', '30000');
 INSERT INTO producto (SKU, nombre, categoria, productor, cantidad_en_stock, Precio) 
-VALUES ('37117', 'DDR4 8GB 2666MHz Vengeance LPX', 'RAM', 'CORSAIR', '10', '31000')
+VALUES ('37117', 'DDR4 8GB 2666MHz Vengeance LPX', 'RAM', 'CORSAIR', '10', '31000');
 INSERT INTO producto (SKU, nombre, categoria, productor, cantidad_en_stock, Precio) 
-VALUES ('46613', 'DDR4 8GB 3600MHz Vengeance LPX', 'RAM', 'CORSAIR', '27', '42000')
+VALUES ('46613', 'DDR4 8GB 3600MHz Vengeance LPX', 'RAM', 'CORSAIR', '27', '42000');
 INSERT INTO producto (SKU, nombre, categoria, productor, cantidad_en_stock, Precio) 
 VALUES ('47666', 'DDR5 8GB 4800MHz Value RAM', 'RAM', 'CRUCIAL', '17', '48000');
+
 -- Evaluamos tabla
-SELECT * FROM cliente;
+SELECT * FROM producto;
