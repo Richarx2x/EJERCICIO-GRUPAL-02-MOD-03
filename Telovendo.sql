83e4bbdf9088bd94d4babfc170e978c51f81db36
Simon Zelada <128656840+supersimonkybone@users.noreply.github.com>
2023-04-16 12:20:56 -0400

    Update Telovendo.sql

--- Telovendo.sql
+++ Telovendo.sql
@@ -1,1 +1,37 @@
+-- RAMA SIMON IMPORTANTE SUBIR PRIMERO YA QUE AGREGA LAS COLUMNAS NUEVAS
 
+-- CREACION DE COLUMNAS NUEVAS
+ALTER TABLE cliente
+ADD COLUMN Total Pagado INT NOT NULL DEFAULT '0';
+
+ALTER TABLE vendedor
+ADD COLUMN Salario INT NOT NULL DEFAULT '410000';
+
+ALTER TABLE Producto
+ADD COLUMN Precio INT NOT NULL DEFAULT '10000';
+
+-- CREACION DE PRODUCTOS NUEVOS SSD
+
+INSERT INTO producto (SKU, nombre, categoria, productor, cantidad_en_stock, Precio) 
+VALUES ('45565', 'Unidad SSD 500GB Sata3 2.5" CS900', 'SSD', 'PNY', '12', '42000')
+INSERT INTO producto (SKU, nombre, categoria, productor, cantidad_en_stock, Precio) 
+VALUES ('27163', 'Unidad SSD 480GB Sata3 2.5" A400" CS900', 'SSD', 'KINGSTON', '14', '32000')
+INSERT INTO producto (SKU, nombre, categoria, productor, cantidad_en_stock, Precio) 
+VALUES ('27162', 'Unidad SSD 240GB Sata3 2.5" A400" CS900', 'SSD', 'KINGSTON', '11', '22000')
+INSERT INTO producto (SKU, nombre, categoria, productor, cantidad_en_stock, Precio) 
+VALUES ('28150', 'Unidad SSD 500GB Sata3 2.5" MX500', 'SSD', 'CRUCIAL', '27', '62000')
+INSERT INTO producto (SKU, nombre, categoria, productor, cantidad_en_stock, Precio) 
+VALUES ('30966', 'Unidad SSD 240GB Sata3 2.5" BX500', 'SSD', 'CRUCIAL', '36', '21000')
+
+-- CREACION DE PRODUCTOS NUEVOS RAM
+
+INSERT INTO producto (SKU, nombre, categoria, productor, cantidad_en_stock, Precio) 
+VALUES ('45565', 'Unidad SSD 500GB Sata3 2.5" CS900', 'SSD', 'PNY', '12', '42000')
+INSERT INTO producto (SKU, nombre, categoria, productor, cantidad_en_stock, Precio) 
+VALUES ('27163', 'Unidad SSD 480GB Sata3 2.5" A400" CS900', 'SSD', 'KINGSTON', '14', '32000')
+INSERT INTO producto (SKU, nombre, categoria, productor, cantidad_en_stock, Precio) 
+VALUES ('27162', 'Unidad SSD 240GB Sata3 2.5" A400" CS900', 'SSD', 'KINGSTON', '11', '22000')
+INSERT INTO producto (SKU, nombre, categoria, productor, cantidad_en_stock, Precio) 
+VALUES ('28150', 'Unidad SSD 500GB Sata3 2.5" MX500', 'SSD', 'CRUCIAL', '27', '62000')
+INSERT INTO producto (SKU, nombre, categoria, productor, cantidad_en_stock, Precio) 
+VALUES ('30966', 'Unidad SSD 240GB Sata3 2.5" BX500', 'SSD', 'CRUCIAL', '36', '21000')
