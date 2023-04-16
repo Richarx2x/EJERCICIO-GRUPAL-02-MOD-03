b6a8ff0b87d04c5a510d0b1100d6bc0b12f94906
Simon Zelada <128656840+supersimonkybone@users.noreply.github.com>
2023-04-16 12:27:38 -0400

    Update Telovendo.sql

--- Telovendo.sql
+++ Telovendo.sql
@@ -26,12 +26,12 @@ VALUES ('30966', 'Unidad SSD 240GB Sata3 2.5" BX500', 'SSD', 'CRUCIAL', '36', '2
 -- CREACION DE PRODUCTOS NUEVOS RAM
 
 INSERT INTO producto (SKU, nombre, categoria, productor, cantidad_en_stock, Precio) 
-VALUES ('45565', 'Unidad SSD 500GB Sata3 2.5" CS900', 'SSD', 'PNY', '12', '42000')
+VALUES ('33918', 'DDR4 4GB 2666MHz Value RAM', 'RAM', 'CRUCIAL', '33', '22000')
 INSERT INTO producto (SKU, nombre, categoria, productor, cantidad_en_stock, Precio) 
-VALUES ('27163', 'Unidad SSD 480GB Sata3 2.5" A400" CS900', 'SSD', 'KINGSTON', '14', '32000')
+VALUES ('43964', 'DDR4 8GB 2666MHz Beast', 'RAM', 'KINGSTON', '28', '30000')
 INSERT INTO producto (SKU, nombre, categoria, productor, cantidad_en_stock, Precio) 
-VALUES ('27162', 'Unidad SSD 240GB Sata3 2.5" A400" CS900', 'SSD', 'KINGSTON', '11', '22000')
+VALUES ('37117', 'DDR4 8GB 2666MHz Vengeance LPX', 'RAM', 'CORSAIR', '10', '31000')
 INSERT INTO producto (SKU, nombre, categoria, productor, cantidad_en_stock, Precio) 
-VALUES ('28150', 'Unidad SSD 500GB Sata3 2.5" MX500', 'SSD', 'CRUCIAL', '27', '62000')
+VALUES ('46613', 'DDR4 8GB 3600MHz Vengeance LPX', 'RAM', 'CORSAIR', '27', '42000')
 INSERT INTO producto (SKU, nombre, categoria, productor, cantidad_en_stock, Precio) 
-VALUES ('30966', 'Unidad SSD 240GB Sata3 2.5" BX500', 'SSD', 'CRUCIAL', '36', '21000')
+VALUES ('47666', 'DDR5 8GB 4800MHz Value RAM', 'RAM', 'CRUCIAL', '17', '48000')
